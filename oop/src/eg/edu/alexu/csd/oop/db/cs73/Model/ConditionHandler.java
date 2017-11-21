3167e4e10ff450df7c1201b5fa514810d232f1b0
Youssef_Ali <youssefali6212@gmail.com>
2017-11-21 14:51:52 +0200

    Infix to postfix conversion is done

--- oop/src/eg/edu/alexu/csd/oop/db/cs73/Model/ConditionHandler.java
+++ oop/src/eg/edu/alexu/csd/oop/db/cs73/Model/ConditionHandler.java
@@ -1,13 +1,96 @@
 package eg.edu.alexu.csd.oop.db.cs73.Model;
 
 import eg.edu.alexu.csd.oop.db.cs73.Model.DBObjects.Column;
+import eg.edu.alexu.csd.oop.db.cs73.Model.DBObjects.Table;
+
+import java.util.ArrayList;
+import java.util.Stack;
 
 public class ConditionHandler {
 
-    public int[] getValidIndicesFrom(Column column, String condition){ // condition:String, after-where part.
-        int[] currentIndices = null;
+    Stack<String> stack;
+
+    public ArrayList<Integer> getValidIndicesFrom(Table table, String condition){ // condition:String, after-where part.
+        ArrayList<Integer> currentIndices = null;
+
+
+
 
 
         return currentIndices;
+    }
+
+    public ArrayList<String> infixToPostfix(ArrayList<String> infix){
+        ArrayList<String> postfix = new ArrayList<>();
+        stack = new Stack<>();
+
+        int openPar = 0;
+        boolean prevIsOpr = false;
+
+        for(String part : infix){
+            if(isOperator(part)){
+                if(prevIsOpr){
+                    throw new RuntimeException("Error: Invalid expression after where cluase. Hint: Check operators.");
+                }
+
+                while(!stack.isEmpty() && precedence(part) <= precedence(stack.peek())){
+                    postfix.add(stack.pop());
+                }
+                stack.push(part);
+                prevIsOpr = true;
+            }
+            else if(part.equalsIgnoreCase("(")){
+                if(prevIsOpr){
+                    throw new RuntimeException("Error: Invalid expression after where cluase. Hint: Check operators.");
+                }
+
+                openPar++;
+                stack.push(part);
+                prevIsOpr = true;
+            }
+            else if(part.equalsIgnoreCase(")")){
+                if(prevIsOpr){
+                    throw new RuntimeException("Error: Invalid expression after where cluase. Hint: Check operators.");
+                }
+                if(openPar == 0){
+                    throw new RuntimeException("Error: Invalid expression after where cluase. Hint: Check parenthesis.");
+                }
+
+                while(!stack.peek().equalsIgnoreCase("(")){
+                    postfix.add(stack.pop());
+                }
+                if(stack.peek().equalsIgnoreCase("(")){
+                    stack.pop();
+                }
+                openPar--;
+                prevIsOpr = true;
+            }
+            else{ // conditional term
+                prevIsOpr = false;
+                postfix.add(part);
+            }
+        }
+
+        while(!stack.isEmpty()){
+            postfix.add(stack.pop());
+        }
+
+        return postfix;
+    }
+
+    //public ArrayList<Integer> evaluate(){}
+
+    private boolean isOperator(String part) {
+        return part.equalsIgnoreCase("not") || part.equalsIgnoreCase("and") || part.equalsIgnoreCase("or");
+    }
+
+    private int precedence(String operator){
+        if(operator.equalsIgnoreCase("not"))
+            return 3;
+        if(operator.equalsIgnoreCase("and"))
+            return 2;
+        if(operator.equalsIgnoreCase("or"))
+            return 1;
+        return -1;
     }
 }
--- oop/src/eg/edu/alexu/csd/oop/db/cs73/View/Testing.java
+++ oop/src/eg/edu/alexu/csd/oop/db/cs73/View/Testing.java
@@ -1,6 +1,7 @@
 package eg.edu.alexu.csd.oop.db.cs73.View;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import eg.edu.alexu.csd.oop.db.Database;
 
@@ -10,19 +11,35 @@ import eg.edu.alexu.csd.oop.db.Database;
 
  */
 
+import eg.edu.alexu.csd.oop.db.cs73.Model.ConditionHandler;
 import eg.edu.alexu.csd.oop.db.cs73.Model.DatabaseImp;
 
 public class Testing {
 
     public static void main(String[] args){
 
-       String x = "UPDATE Customers SET ContactName = 'Alfred Schmidt', City = 'Frankfurt' WHERE CustomerID = 1;" ;
+       /*String x = "UPDATE Customers SET ContactName = 'Alfred Schmidt', City = 'Frankfurt' WHERE CustomerID = 1;" ;
         x = x.replaceAll("\\)", " ").replaceAll("\\(", " ").replaceAll("'", "").replaceAll("=", " = ").replaceAll("\\s+\\,", ",");
         System.out.println(x);
         String [] z = x.split("\\s+|\\,\\s*|\\(|\\)");
         for(String v : z)
-        	System.out.println(v);
+        	System.out.println(v);*/
 
+        ConditionHandler ch = new ConditionHandler();
+        ArrayList<String> arr = new ArrayList<>();
+        /*arr.add("not");
+        arr.add("(");
+        arr.add("a = 5");
+        arr.add("and");
+        arr.add("b > 6");
+        arr.add(")");*/
+        arr.add("a = 5");
+        arr.add("and");
+        arr.add("b > 5");
+        arr.add("or");
+        arr.add("c < 5");
+        arr = ch.infixToPostfix(arr);
+        System.out.println(arr);
     }
 
 }
