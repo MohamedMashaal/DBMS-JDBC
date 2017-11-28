f758489e7ec225ccb5cacee42d597554c582351a
MohamedMashaal <mohamedmashaal1997@gmail.com>
2017-11-28 18:13:55 +0200

    TS

--- oop/src/eg/edu/alexu/csd/oop/db/cs73/Model/DirectoryHandler.java
+++ oop/src/eg/edu/alexu/csd/oop/db/cs73/Model/DirectoryHandler.java
@@ -13,7 +13,7 @@ public class DirectoryHandler {
     XMLParser xmlParser;
 
     public DirectoryHandler(){
-        mainDirectory = new File("data");
+        mainDirectory = new File("Database");
         mainDirectory.mkdirs();
         xmlParser = new XMLParser();
     }
