2c402bf1f4d6787348964ab9f35bffed6b6af293
Youssef_Ali <youssefali6212@gmail.com>
2017-11-29 00:34:41 +0200

    Adding Connection and Driver Implementation Classes

---
+++ oop/src/eg/edu/alexu/csd/oop/jdbc/cs73/DriverImp.java
@@ -0,0 +1,42 @@
+package eg.edu.alexu.csd.oop.jdbc.cs73;
+
+import java.sql.*;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class DriverImp implements Driver {
+    @Override
+    public Connection connect(String s, Properties properties) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean acceptsURL(String s) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public DriverPropertyInfo[] getPropertyInfo(String s, Properties properties) throws SQLException {
+        return new DriverPropertyInfo[0];
+    }
+
+    @Override
+    public int getMajorVersion() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getMinorVersion() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean jdbcCompliant() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        throw new UnsupportedOperationException();
+    }
+}
