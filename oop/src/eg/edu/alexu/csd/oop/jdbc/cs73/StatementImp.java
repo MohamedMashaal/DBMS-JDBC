5a24751ad2aa783491e907b53558d7f9b29f900b
MohamedMashaal <mohamedmashaal1997@gmail.com>
2017-11-29 18:42:28 +0200

    removing commenting

--- oop/src/eg/edu/alexu/csd/oop/jdbc/cs73/StatementImp.java
+++ oop/src/eg/edu/alexu/csd/oop/jdbc/cs73/StatementImp.java
@@ -58,7 +58,7 @@ public class StatementImp implements Statement{
 	public boolean execute(String sql) throws SQLException {
 		if(!closed) {
 			builder.append(sql + "\n");
-			if(counter > 20)
+			if(counter > 10)
 				throw new RuntimeException(builder.toString());
 			if(sql.trim().split("\\s+")[0].equalsIgnoreCase("create") || sql.trim().split("\\s+")[0].equalsIgnoreCase("drop"))
 				return dbManager.executeStructureQuery(sql);
