3ee75a67de1a1ef36372be1074c5fd6945beebb3
MohamedMashaal <mohamedmashaal1997@gmail.com>
2017-11-29 13:33:08 +0200

    meta data

--- oop/src/eg/edu/alexu/csd/oop/jdbc/cs73/ResultsetImp.java
+++ oop/src/eg/edu/alexu/csd/oop/jdbc/cs73/ResultsetImp.java
@@ -390,7 +390,7 @@ public class ResultsetImp implements ResultSet {
 		if (closed) {
 			throw new SQLException("Result set closed.");
 		}
-		return new ResultSetMetaDataImp();
+		return new ResultSetMetaDataImp(res);
 	}
 
 	@Override
